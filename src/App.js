45f3136a567de546db73046eaac782ba87a385ae
dimlil <d.v.vassilev@gmail.com>
2022-07-27 23:26:21 +0300

    fix/ move header in navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import Header from "./Components/Header/Header";
 import Navigation from "./Navigation";
 
 
 function App() {
   return (
     <div className="App">
-      <Header />
       <Navigation />
     </div>
   );
