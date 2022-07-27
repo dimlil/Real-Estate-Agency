aa2379fdf2e1a3075c4f13873437a82e475567ca
dimlil <d.v.vassilev@gmail.com>
2022-07-27 23:26:43 +0300

    feat/ added route

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,14 +1,18 @@
-import {styles} from './Header.module.css'
-function Header(){
-    return(
+import { Link } from 'react-router-dom'
+import { styles } from './Header.module.css'
+function Header() {
+    return (
         <nav>
-            <a href="#">Home</a>
+            {/* <a href="#">Home</a> */}
+            <Link to={'/'}>Home</Link>
             <ul>
-                <li><a href="#">Housing for rent</a></li>
-                
+                {/* <li><a href="#">Housing for rent</a></li> */}
+            <Link to={'/housing-for-rent'}>Housing for rent</Link>
+
+
                 <li><a href="#">Login</a></li>
                 <li><a href="#">Register</a></li>
-                
+
                 <li><a href="#">Create offer</a></li>
                 <li><a href="#">Search</a></li>
                 <li><a href="#">Logout</a></li>
--- src/Navigation.js
+++ src/Navigation.js
@@ -1,10 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Header from './Components/Header/Header'
 import HomePage from './Pages/HomePage/HomePage'
+import HouseForRent from './Pages/HouseForRent/HouseForRent'
 function Navigation() {
     return (
         <BrowserRouter>
+        <Header />
             <Routes >
                 <Route path="/" element={<HomePage />} />
+                <Route path="/housing-for-rent" element={<HouseForRent />} />
             </Routes>
         </BrowserRouter>
     )
