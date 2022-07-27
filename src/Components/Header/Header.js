94c9f5718cabc7594b17413b94d7d101f49ca1c7
dimlil <d.v.vassilev@gmail.com>
2022-07-27 23:29:59 +0300

    fix/ remove warning for not using variable

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom'
-import { styles } from './Header.module.css'
+import { } from './Header.module.css'
 function Header() {
     return (
         <nav>
@@ -7,7 +7,7 @@ function Header() {
             <Link to={'/'}>Home</Link>
             <ul>
                 {/* <li><a href="#">Housing for rent</a></li> */}
-            <Link to={'/housing-for-rent'}>Housing for rent</Link>
+                <Link to={'/housing-for-rent'}>Housing for rent</Link>
 
 
                 <li><a href="#">Login</a></li>
