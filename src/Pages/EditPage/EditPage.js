72349c490b864f9b4806774499099fe1e42f6fa3
dimlil <d.v.vassilev@gmail.com>
2022-08-06 20:49:37 +0300

    fix/ change class on className

--- src/Pages/EditPage/EditPage.js
+++ src/Pages/EditPage/EditPage.js
@@ -66,49 +66,49 @@ function EditPage() {
     }
     return (
         <section id="edit-page">
-            <div class={styles.editContainer}>
-                <div class={styles.boxImage}>
+            <div className={styles.editContainer}>
+                <div className={styles.boxImage}>
 
                 </div>
-                <h2 class={styles.boxHeading}>
+                <h2 className={styles.boxHeading}>
                     Edit Home
                 </h2>
 
-                <form class={styles.form} action="" method="">
-                    <div class={styles.input}>
-                        <input type="text" class={styles.inputField} id="home-name" name="" value={name} onChange={updatingName} />
-                        <label class={styles.homeName}>Name:</label>
+                <form className={styles.form} action="" method="">
+                    <div className={styles.input}>
+                        <input type="text" className={styles.inputField} id="home-name" name="" value={name} onChange={updatingName} />
+                        <label className={styles.homeName}>Name:</label>
                     </div>
-                    <div class={styles.input}>
-                        <input type="text" class={styles.inputField} id="type" name="" value={type} onChange={updatingType} />
-                        <label class={styles.type}>Property Type:</label>
+                    <div className={styles.input}>
+                        <input type="text" className={styles.inputField} id="type" name="" value={type} onChange={updatingType} />
+                        <label className={styles.type}>Property Type:</label>
                     </div>
-                    <div class={styles.input}>
-                        <input type="number" class={styles.inputField} id="year" name="" value={year} onChange={updatingYear} />
-                        <label class={styles.year}>Year Built:</label>
+                    <div className={styles.input}>
+                        <input type="number" className={styles.inputField} id="year" name="" value={year} onChange={updatingYear} />
+                        <label className={styles.year}>Year Built:</label>
                     </div>
-                    <div class={styles.input}>
-                        <input type="text" class={styles.inputField} id="city" name="" value={city} onChange={updatingCity} />
-                        <label class={styles.city}>City</label>
-                    </div>
-
-                    <div class={styles.input}>
-                        <input type="text" class={styles.inputField} id="homeImage" name="" value={imgUrl} onChange={updatingImgUrl} />
-                        <label class={styles.homeImage}>Home Image:</label>
+                    <div className={styles.input}>
+                        <input type="text" className={styles.inputField} id="city" name="" value={city} onChange={updatingCity} />
+                        <label className={styles.city}>City</label>
                     </div>
 
-                    <div class={styles.input}>
-                        <input type="text" class={styles.inputField} id="description" name="" value={propertyDesc} onChange={updatingPropertyDesc} />
-                        <label class={styles.description}>Property Description:</label>
+                    <div className={styles.input}>
+                        <input type="text" className={styles.inputField} id="homeImage" name="" value={imgUrl} onChange={updatingImgUrl} />
+                        <label className={styles.homeImage}>Home Image:</label>
                     </div>
 
-                    <div class={styles.input}>
-                        <input type="number" class={styles.inputField} id="availablePieces" name="" value={availablePieces} onChange={updatingAvailablePieces} />
-                        <label class={styles.availablePieces}>Available pieces:</label>
+                    <div className={styles.input}>
+                        <input type="text" className={styles.inputField} id="description" name="" value={propertyDesc} onChange={updatingPropertyDesc} />
+                        <label className={styles.description}>Property Description:</label>
                     </div>
 
-                    <div class={styles.createAction}>
-                        <button class={styles.createButton}>Edit</button>
+                    <div className={styles.input}>
+                        <input type="number" className={styles.inputField} id="availablePieces" name="" value={availablePieces} onChange={updatingAvailablePieces} />
+                        <label className={styles.availablePieces}>Available pieces:</label>
+                    </div>
+
+                    <div className={styles.createAction}>
+                        <button className={styles.createButton}>Edit</button>
                     </div>
                 </form>
             </div>
