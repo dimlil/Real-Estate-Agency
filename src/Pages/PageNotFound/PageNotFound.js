6a61c61f3b72d7755b4c7beadb3479966fb92166
dimlil <d.v.vassilev@gmail.com>
2022-08-12 21:07:42 +0300

    fix/ added link

--- src/Pages/PageNotFound/PageNotFound.js
+++ src/Pages/PageNotFound/PageNotFound.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import styles from './PageNotFound.module.css'
 function PageNotFound(){
     return(
@@ -5,7 +6,7 @@ function PageNotFound(){
             <div className={styles.notFoundContainer}>
                 <h1>404</h1>
                 <h2>Page Not Found</h2>
-                <p>The Page you are looking for doesn't exist or another error occurred. Go to <a href="#">Home Page.</a></p>
+                <p>The Page you are looking for doesn't exist or another error occurred. Go to <Link to={'/'}> Home Page.</Link></p>
             </div>
         </main>
     )
